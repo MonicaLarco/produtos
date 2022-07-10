6a55c423233b46e6e1c6147aa1ce7c8762fd1179
mlarco <mlarco94@gmail.com>
2022-07-10 18:58:13 -0300

    Produtos api created

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Product list
 const lista_produtos = {
     produtos: [
         { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  },
@@ -10,3 +14,37 @@ const lista_produtos = {
         { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
     ]
 };
+
+
+app.listen(port, () => console.info(`Listening on ${ port }`));
+
+// Get Product list
+app.get('/produtos', function(req, res) {
+    res.json(lista_produtos);
+});
+
+//Get Product ID
+app.get('/produtos/:id', function(req, res) {
+    let id = Number.parseInt(req.params.id);
+    let idx = lista_produtos.produtos.findIndex((elem) => elem.id == id);
+    if (idx > -1) {
+        res.json(lista_produtos.produtos[idx]);
+    } else {
+        res.status(404).json({message: "Produto não encontrado"});
+    }
+});
+
+// Add a new product
+app.post('/produtos', urlencodedParser, function(req, res) {
+    if (!req.body){
+        res.status(404);
+        res.send('Error');
+    } else {
+        lista_produtos.produtos.push(req.body);
+        res.send(`New product details: ${req.body}`);
+    }
+});
+
+// Delete a Product
+
+
